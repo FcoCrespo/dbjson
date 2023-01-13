6a3034cc2b4d62846c3e2ca4fcd735fbd1e45042
FcoCrespo <Fco.Crespo@alu.uclm.es>
2023-01-13 13:48:22 +0100

    Create inventario.js

---
+++ inventario.js
@@ -0,0 +1,41 @@
+{
+  "status": "S",
+  "codigo": "163160",
+  "doc_inventario": "2022120700",
+  "fab": "CONDUCTOR RVK, ENERGIA COBRE",
+  "ref_fab": "RZ1-K 4X16",
+  "suj_lote": "X",
+  "lotes": [
+    "ALOTE1",
+    "ALOTE2",
+    "ALOTE3"
+  ],
+  "tipo_stk_ad": [
+    "I",
+    "B"
+  ],
+  "unidad": "M",
+  "descripcion": "CABLE RZ1-K CU C/H 1KV 4X16",
+  "recuentos": [
+      {
+        "fecha": "2022,191",
+        "hora": "165012",
+        "conteo": "700.000",
+        "lote": "ALOTE1",
+        "tipo_stk_ad": "I"   
+      },
+      {
+        "fecha": "2022,191",
+        "hora": "165012",
+        "conteo": "1000.000",
+        "lote": "ALOTE2",
+        "tipo_stk_ad": "B"
+      },
+      {
+        "fecha": "2022,191",
+        "hora": "165012",
+        "conteo": "1500.000",
+        "lote": "ALOTE2"
+      }
+  ]
+}
